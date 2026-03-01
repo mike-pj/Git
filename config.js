28e51df318c695811eaa0992e54f9053a36d739e
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2026-03-01 22:32:57 +0100

    initial commit restored

--- config.js
+++ config.js
@@ -1,9 +1,2 @@
-// console.log('config.js');
-// console.log('version1');
-
-//Completing the Version History
-// console.log('config.js');
-// console.log('version 2');
-
 console.log('config.js');
-console.log('version 3');
+console.log('version1');
--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-// console.log('config.js');
-// console.log('version1');
-
-//Completing the Version History
-// console.log('index.js');
-// console.log('version 2');
-
 console.log('index.js');
-console.log('version 3');
-
-//example check previous version, Version 2
-// git checkout 50cc117a95f2eef06d626e4db4b7571f6f7d1d54
-// the long number above is called the 'commit hash' and it is unique to each commit. You can find it in the git log. 
+console.log('version1');
